66001039935e5e7f9a9043a2e19f28802074e797
quesada03 <dazamariana0301@gmail.com>
2025-09-17 15:02:56 -0500

    Definimos las variables y dimos la bienvenida

---
+++ Ejercicio-1.cpp
@@ -0,0 +1,11 @@
+/*
+Mariana Daza
+Samuel Quesada
+*/
+#include <iostream>
+int main() {
+    int vec[5];
+    int suma=0;
+    int i=0;
+    std::cout<<"Bienvenido al programa que suma"<<std::endl;
+}
